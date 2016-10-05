3fe7b1aaa9c19babdb25d5fea9fda4c7e8127d8b
Jacobi Carter <cobi@jacobicarter.com>
2016-10-04 17:02:27 -0700

    Initial things

---
+++ twitchbot-common/src/main/java/tv/twitchbot/common/dto/Message.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.common.dto;
+
+/**
+ * Created by cobi on 10/4/16.
+ */
+public abstract class Message {
+    abstract String serialize();
+    
+}
---
+++ twitchbot-common/src/main/java/tv/twitchbot/common/modules/Module.java
@@ -0,0 +1,8 @@
+package tv.twitchbot.common.modules;
+
+/**
+ * Created by cobi on 10/4/16.
+ */
+public interface Module {
+
+}
