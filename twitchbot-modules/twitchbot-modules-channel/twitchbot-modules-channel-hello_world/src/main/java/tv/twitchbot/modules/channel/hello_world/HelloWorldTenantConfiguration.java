f73396f76756b183bb1f3485316745c8483b0b50
Josh Johnson <SnoFox@SnoFox.net>
2016-10-06 16:22:45 -0700

    Hello, world!

---
+++ twitchbot-modules/twitchbot-modules-channel/twitchbot-modules-channel-hello_world/src/main/java/tv/twitchbot/modules/channel/hello_world/HelloWorldTenantConfiguration.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.modules.channel.hello_world;
+
+import tv.twitchbot.common.modules.TenantConfiguration;
+
+/**
+ * Created by Josh on 2016-10-06.
+ */
+public class HelloWorldTenantConfiguration extends TenantConfiguration {
+}
