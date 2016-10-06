f1367c97612685fd81f8339f2be7bc8ab0dde5c0
Josh Johnson <SnoFox@SnoFox.net>
2016-10-06 16:22:45 -0700

    Hello, world!

---
+++ twitchbot-modules/twitchbot-modules-channel/twitchbot-modules-channel-hello_world/src/main/java/tv/twitchbot/modules/channel/hello_world/HelloWorldSettings.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.modules.channel.hello_world;
+
+import tv.twitchbot.common.modules.ModuleSettings;
+
+/**
+ * Created by Josh on 2016-10-06.
+ */
+public class HelloWorldSettings extends ModuleSettings {
+}
