83ad880d387d621f0d431ae8e6dc79861fa88ca5
Jacobi Carter <cobi@jacobicarter.com>
2016-10-06 02:39:04 -0400

    More framework!

--- twitchbot-common/src/main/java/tv/twitchbot/common/services/queue/MessageQueue.java
+++ twitchbot-common/src/main/java/tv/twitchbot/common/services/queue/MessageQueue.java
@@ -1,11 +1,12 @@
 package tv.twitchbot.common.services.queue;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import tv.twitchbot.common.dto.messages.Message;
 
 /**
  * Created by cobi on 10/5/16.
  */
 public interface MessageQueue {
-    Message get();
+    Message get() throws InterruptedException, InvalidProtocolBufferException;
     void add(Message message);
 }
