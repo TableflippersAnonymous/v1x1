92deb447a95b0f92d569a1edd4f1ac3f3607740b
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-10-16 18:43:03 -0400

    -TenantStore +Deduplicator +DAOManager

---
+++ twitchbot-common/src/main/java/tv/twitchbot/common/services/persistence/DAOManager.java
@@ -0,0 +1,33 @@
+package tv.twitchbot.common.services.persistence;
+
+import com.datastax.driver.mapping.MappingManager;
+import tv.twitchbot.common.dao.DAOGlobalUser;
+import tv.twitchbot.common.dao.DAOTenant;
+import tv.twitchbot.common.dao.DAOTenantUserPermissions;
+
+/**
+ * Created by naomi on 10/16/2016.
+ */
+public class DAOManager {
+    private DAOTenant daoTenant;
+    private DAOGlobalUser daoGlobalUser;
+    private DAOTenantUserPermissions daoTenantUserPermissions;
+
+    public DAOManager(MappingManager mappingManager) {
+        daoTenant = new DAOTenant(mappingManager);
+        daoGlobalUser = new DAOGlobalUser(mappingManager);
+        daoTenantUserPermissions = new DAOTenantUserPermissions(mappingManager);
+    }
+
+    public DAOTenant getDaoTenant() {
+        return daoTenant;
+    }
+
+    public DAOGlobalUser getDaoGlobalUser() {
+        return daoGlobalUser;
+    }
+
+    public DAOTenantUserPermissions getDaoTenantUserPermissions() {
+        return daoTenantUserPermissions;
+    }
+}
---
+++ twitchbot-common/src/main/java/tv/twitchbot/common/services/persistence/Deduplicator.java
@@ -0,0 +1,32 @@
+package tv.twitchbot.common.services.persistence;
+
+import org.redisson.api.RSetCache;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.ByteArrayCodec;
+import tv.twitchbot.common.dto.core.Module;
+import tv.twitchbot.common.dto.core.UUID;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by naomi on 10/16/2016.
+ */
+public class Deduplicator {
+    private RSetCache<byte[]> setCache;
+
+    public Deduplicator(RedissonClient client, Module module) {
+        this(client, "Module|" + module.getName());
+    }
+
+    public Deduplicator(RedissonClient client, String name) {
+        setCache = client.getSetCache("Deduplicator|" + name, ByteArrayCodec.INSTANCE);
+    }
+
+    public boolean seenAndAdd(UUID uuid) {
+        byte[] bytes = uuid.toProto().toByteArray();
+        if(setCache.contains(bytes))
+            return true;
+        setCache.add(bytes, 5, TimeUnit.MINUTES);
+        return false;
+    }
+}
