ec3948cc2e9305e3c1f7fe310d5dd1d1197dcc9c
Cobi <cobi@jacobicarter.com>
2016-10-16 21:51:09 -0400

    +EventRouter

---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-event_router/src/main/java/tv/twitchbot/modules/core/eventrouter/EventRouterTenantConfiguration.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.modules.core.eventrouter;
+
+import tv.twitchbot.common.modules.TenantConfiguration;
+
+/**
+ * Created by cobi on 10/16/2016.
+ */
+public class EventRouterTenantConfiguration extends TenantConfiguration {
+}
