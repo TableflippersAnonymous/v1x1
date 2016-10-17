f353c95db3b4ea44647c6daca9f1b74b51a79408
Josh Johnson <SnoFox@SnoFox.net>
2016-10-17 01:56:23 -0700

    Update build and dev files

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ echo "Finding modules..."
 while read module; do
   MODULES="$MODULES v1x1-$module"
 done < <(find twitchbot-modules -mindepth 2 -iname "twitchbot-modules-*" -type d -printf '%f\n'|cut -f4- -d- --)
+echo "Clearing out old Docker containers..."
+docker-compose down
 echo "Clearing out old Docker images..."
 docker rmi ${MODULES}
 
