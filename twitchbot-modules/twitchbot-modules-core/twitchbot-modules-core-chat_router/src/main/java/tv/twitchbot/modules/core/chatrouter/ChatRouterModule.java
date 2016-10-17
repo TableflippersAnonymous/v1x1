437dd148b9a1b8e4852db247e26d0b4a41b53156
Jacobi Carter <cobi@jacobicarter.com>
2016-10-16 21:14:41 -0400

    +ChatRouter

---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-chat_router/src/main/java/tv/twitchbot/modules/core/chatrouter/ChatRouterModule.java
@@ -0,0 +1,23 @@
+package tv.twitchbot.modules.core.chatrouter;
+
+import tv.twitchbot.common.modules.ServiceModule;
+
+/**
+ * Created by cobi on 10/16/2016.
+ */
+public class ChatRouterModule extends ServiceModule<ChatRouterSettings, ChatRouterGlobalConfiguration, ChatRouterTenantConfiguration> {
+    @Override
+    public String getName() {
+        return "ChatRouter";
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        registerService(new ChatRouterService(this));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ChatRouterModule().entryPoint(args);
+    }
+}
---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-chat_router/src/main/java/tv/twitchbot/modules/core/chatrouter/TmiServiceClient.java
@@ -0,0 +1,33 @@
+package tv.twitchbot.modules.core.chatrouter;
+
+import tv.twitchbot.common.dto.core.Channel;
+import tv.twitchbot.common.dto.messages.requests.SendMessageRequest;
+import tv.twitchbot.common.dto.messages.responses.SendMessageResponse;
+import tv.twitchbot.common.modules.GlobalConfiguration;
+import tv.twitchbot.common.modules.Module;
+import tv.twitchbot.common.modules.ModuleSettings;
+import tv.twitchbot.common.modules.TenantConfiguration;
+import tv.twitchbot.common.rpc.client.ServiceClient;
+
+import java.util.concurrent.Future;
+
+/**
+ * Created by cobi on 10/16/2016.
+ */
+public class TmiServiceClient extends ServiceClient<SendMessageRequest, SendMessageResponse> {
+    private String channel;
+
+    public TmiServiceClient(Module<? extends ModuleSettings, ? extends GlobalConfiguration, ? extends TenantConfiguration> module, String channel) {
+        super(module, SendMessageResponse.class);
+        this.channel = channel;
+    }
+
+    @Override
+    protected String getServiceName() {
+        return "Core|TMI|channel|" + channel;
+    }
+
+    public Future<SendMessageResponse> sendMessage(Channel channel, String text) {
+        return send(new SendMessageRequest(getModule(), getQueueName(), channel, text));
+    }
+}
