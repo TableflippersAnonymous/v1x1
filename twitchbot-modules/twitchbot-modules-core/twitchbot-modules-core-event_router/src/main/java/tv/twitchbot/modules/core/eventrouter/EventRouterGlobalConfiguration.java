2aefad15a57950f3eabc5c37dc7bc32fd19a08bd
Jacobi Carter <cobi@jacobicarter.com>
2016-10-16 21:51:09 -0400

    +EventRouter

---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-event_router/src/main/java/tv/twitchbot/modules/core/eventrouter/EventRouterGlobalConfiguration.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.modules.core.eventrouter;
+
+import tv.twitchbot.common.modules.GlobalConfiguration;
+
+/**
+ * Created by cobi on 10/16/2016.
+ */
+public class EventRouterGlobalConfiguration extends GlobalConfiguration {
+}
