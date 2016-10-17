939419c0e97a9e601bad7989993d78560de40afe
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-10-16 21:51:09 -0400

    +EventRouter

---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-event_router/src/main/java/tv/twitchbot/modules/core/eventrouter/EventRouterModule.java
@@ -0,0 +1,55 @@
+package tv.twitchbot.modules.core.eventrouter;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import tv.twitchbot.common.dto.core.Module;
+import tv.twitchbot.common.dto.messages.Event;
+import tv.twitchbot.common.dto.messages.Message;
+import tv.twitchbot.common.modules.ThreadedModule;
+
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by naomi on 10/16/2016.
+ */
+public class EventRouterModule extends ThreadedModule<EventRouterSettings, EventRouterGlobalConfiguration, EventRouterTenantConfiguration> {
+    LoadingCache<String, Set<Module>> moduleCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(2, TimeUnit.SECONDS)
+            .build(new CacheLoader<String, Set<Module>>() {
+                @Override
+                public Set<Module> load(String s) throws Exception {
+                    return getModuleRegistry().modules();
+                }
+            });
+
+    @Override
+    protected void processMessage(Message message) {
+        if(!(message instanceof Event))
+            return;
+
+        try {
+            Set<Module> modules = moduleCache.get("modules");
+            for(Module module : modules)
+                send(getMainQueueForModule(module), message);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "EventRouter";
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EventRouterModule().entryPoint(args);
+    }
+}
---
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-event_router/src/main/java/tv/twitchbot/modules/core/eventrouter/EventRouterSettings.java
@@ -0,0 +1,9 @@
+package tv.twitchbot.modules.core.eventrouter;
+
+import tv.twitchbot.common.modules.ModuleSettings;
+
+/**
+ * Created by naomi on 10/16/2016.
+ */
+public class EventRouterSettings extends ModuleSettings {
+}
