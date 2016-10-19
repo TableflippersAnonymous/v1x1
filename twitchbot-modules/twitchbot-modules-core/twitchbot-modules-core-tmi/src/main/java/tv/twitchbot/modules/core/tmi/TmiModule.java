2036c04852edc46978ec1fdfb1a784cc221b7834
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-10-19 04:16:10 -0400

    Logging, also fix where bot not created

--- twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-tmi/src/main/java/tv/twitchbot/modules/core/tmi/TmiModule.java
+++ twitchbot-modules/twitchbot-modules-core/twitchbot-modules-core-tmi/src/main/java/tv/twitchbot/modules/core/tmi/TmiModule.java
@@ -145,6 +145,7 @@ public class TmiModule extends ServiceModule<TmiSettings, TmiGlobalConfiguration
     private void join(String channel) {
         if(bots.containsKey(channel))
             return;
+        System.out.println("Joining " + channel);
         Tenant tenant = getTenant(channel);
         TmiTenantConfiguration tenantConfiguration = getTenantConfiguration(tenant);
         String oauthToken;
@@ -158,6 +159,7 @@ public class TmiModule extends ServiceModule<TmiSettings, TmiGlobalConfiguration
         }
         RateLimiter messageLimiter = new LocalRateLimiter(scheduledExecutorService, 18, 30);
         TmiBot tmiBot = new TmiBot(username, oauthToken, eventRouter, toDto(), joinLimiter, messageLimiter, getDeduplicator(), this, channel);
+        scheduledExecutorService.submit(tmiBot);
         TmiBot oldTmiBot = bots.put(channel, tmiBot);
         try {
             if(oldTmiBot != null)
