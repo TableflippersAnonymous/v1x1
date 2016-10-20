0f371e80702ab7319e3603e00e1d7d8c3d385c9d
Josh Johnson <SnoFox@SnoFox.net>
2016-10-20 00:53:55 -0700

    Fix EventHandler + RegisteredThreadedModules

--- twitchbot-common/src/main/java/tv/twitchbot/common/modules/eventhandler/EventHandler.java
+++ twitchbot-common/src/main/java/tv/twitchbot/common/modules/eventhandler/EventHandler.java
@@ -1,7 +1,14 @@
 package tv.twitchbot.common.modules.eventhandler;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Josh
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 }
