a90ed70b8af13354b44553d05b58066ec4a78234
Josh Johnson <SnoFox@SnoFox.net>
2016-10-19 21:42:19 -0700

    Better relaunch script

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+getBoolAnswer() {
+    while true; do
+        read -p "$@ [y/n] " -n1
+        printf "\n" >&2
+        REPLY=$(echo $REPLY|tr [A-Z] [a-z])
+        if [ $REPLY == "y" ] || [ $REPLY == "n" ]; then
+            echo $REPLY
+            return
+        fi
+    done
+}
 APP_ENV=$1
 if [ x${APP_ENV} == 'x' ]; then
   echo "Usage: ./build.sh <dev|prod>"
@@ -17,3 +28,10 @@ docker rmi ${MODULES}
 
 echo "Rebuilding..."
 MAVEN_OPTS=-Djava.io.tmpdir=${HOME}/tmp mvn clean install -Denvironmnent=$APP_ENV
+
+if [ $(getBoolAnswer "Run new build?") == "y" ]; then
+    docker-compose up -d cass1 redis zoo1
+    echo "Waiting for core services to start..."
+    sleep 45
+    docker-compose up -d
+fi
