f52577795596de45aa2b8909b46c442307269be4
Josh Johnson <SnoFox@SnoFox.net>
2016-10-22 02:27:31 -0700

    WIP Link Purger

---
+++ twitchbot-common/src/main/java/tv/twitchbot/common/services/chat/ChatException.java
@@ -0,0 +1,10 @@
+package tv.twitchbot.common.services.chat;
+
+/**
+ * @author Josh
+ */
+public class ChatException extends Exception {
+    ChatException(String message) {
+        super();
+    }
+}
