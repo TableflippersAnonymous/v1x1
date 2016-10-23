a0b9b8963cdb67bfa8a543ac231ba2289d167e4f
Jacobi Carter <cobi@jacobicarter.com>
2016-10-23 08:30:16 -0400

    +Scheduler

--- v1x1-modules/v1x1-modules-core/v1x1-modules-core-scheduler/src/main/java/tv/v1x1/modules/core/scheduler/SchedulerService.java
+++ v1x1-modules/v1x1-modules-core/v1x1-modules-core-scheduler/src/main/java/tv/v1x1/modules/core/scheduler/SchedulerService.java
@@ -1,5 +1,10 @@
 package tv.v1x1.modules.core.scheduler;
 
+import com.datastax.driver.mapping.MappingManager;
+import org.redisson.api.RedissonClient;
+import tv.v1x1.common.dto.messages.requests.CronScheduleRequest;
+import tv.v1x1.common.dto.messages.requests.DelayScheduleRequest;
+import tv.v1x1.common.dto.messages.requests.IntervalScheduleRequest;
 import tv.v1x1.common.dto.messages.requests.ScheduleRequest;
 import tv.v1x1.common.dto.messages.responses.ScheduleResponse;
 import tv.v1x1.common.modules.GlobalConfiguration;
@@ -7,17 +12,73 @@ import tv.v1x1.common.modules.Module;
 import tv.v1x1.common.modules.ModuleSettings;
 import tv.v1x1.common.modules.TenantConfiguration;
 import tv.v1x1.common.rpc.services.ThreadedService;
+import tv.v1x1.common.services.queue.MessageQueue;
+import tv.v1x1.modules.core.scheduler.dao.DAOCronSchedule;
+import tv.v1x1.modules.core.scheduler.dao.DAOIntervalSchedule;
+import tv.v1x1.modules.core.scheduler.schedulers.CronScheduler;
+import tv.v1x1.modules.core.scheduler.schedulers.DelayScheduler;
+import tv.v1x1.modules.core.scheduler.schedulers.IntervalScheduler;
+
+import java.util.Date;
 
 /**
  * Created by cobi on 10/22/2016.
  */
 public class SchedulerService extends ThreadedService<ScheduleRequest, ScheduleResponse> {
-    public SchedulerService(final Module<? extends ModuleSettings, ? extends GlobalConfiguration, ? extends TenantConfiguration> module, final String serviceName, final Class<ScheduleRequest> requestClass) {
-        super(module, serviceName, requestClass);
+    private final CronScheduler cronScheduler;
+    private final DelayScheduler delayScheduler;
+    private final IntervalScheduler intervalScheduler;
+
+    public SchedulerService(final Module<? extends ModuleSettings, ? extends GlobalConfiguration, ? extends TenantConfiguration> module) {
+        super(module, "Scheduler", ScheduleRequest.class);
+        final MessageQueue messageQueue = module.getMessageQueueManager().forName(Module.getMainQueueForModule(new tv.v1x1.common.dto.core.Module("event_router")));
+        final MappingManager mappingManager = module.getMappingManager();
+        final RedissonClient client = module.getRedisson();
+        delayScheduler = new DelayScheduler(client, messageQueue, module.toDto());
+        cronScheduler = new CronScheduler(delayScheduler, new DAOCronSchedule(mappingManager), client);
+        intervalScheduler = new IntervalScheduler(delayScheduler, new DAOIntervalSchedule(mappingManager), client);
+    }
+
+    @Override
+    public void start() {
+        delayScheduler.start();
+        cronScheduler.start();
+        intervalScheduler.start();
+        super.start();
     }
 
     @Override
     protected ScheduleResponse call(final ScheduleRequest request) {
-        return null;
+        if(request instanceof CronScheduleRequest)
+            return callInternal((CronScheduleRequest) request);
+        if(request instanceof DelayScheduleRequest)
+            return callInternal((DelayScheduleRequest) request);
+        if(request instanceof IntervalScheduleRequest)
+            return callInternal((IntervalScheduleRequest) request);
+        throw new IllegalArgumentException("Unknown Schedule Request type: " + request.getClass().getCanonicalName());
+    }
+
+    private ScheduleResponse callInternal(final CronScheduleRequest request) {
+        cronScheduler.schedule(request.getMinute(), request.getHour(), request.getDayOfMonth(), request.getMonth(),
+                request.getDayOfWeek(), request.getFrom(), request.getScheduleId(), request.getPayload());
+        return new ScheduleResponse(getModule().toDto(), request.getMessageId());
+    }
+
+    private ScheduleResponse callInternal(final DelayScheduleRequest request) {
+        delayScheduler.schedule(new Date().getTime() + request.getDelay(), request.getFrom(), request.getScheduleId(), request.getPayload());
+        return new ScheduleResponse(getModule().toDto(), request.getMessageId());
+    }
+
+    private ScheduleResponse callInternal(final IntervalScheduleRequest request) {
+        intervalScheduler.schedule(request.getDelay(), request.getFrom(), request.getScheduleId(), request.getPayload());
+        return new ScheduleResponse(getModule().toDto(), request.getMessageId());
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        cronScheduler.shutdown();
+        intervalScheduler.shutdown();
+        delayScheduler.shutdown();
     }
 }
---
+++ v1x1-modules/v1x1-modules-core/v1x1-modules-core-scheduler/src/main/java/tv/v1x1/modules/core/scheduler/dao/DAOIntervalSchedule.java
@@ -0,0 +1,39 @@
+package tv.v1x1.modules.core.scheduler.dao;
+
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
+import com.datastax.driver.mapping.annotations.Accessor;
+import com.datastax.driver.mapping.annotations.Query;
+import tv.v1x1.modules.core.scheduler.dto.IntervalSchedule;
+
+/**
+ * Created by cobi on 10/23/2016.
+ */
+public class DAOIntervalSchedule {
+    private final Mapper<IntervalSchedule> mapper;
+    private final IntervalScheduleAccessor accessor;
+
+    @Accessor
+    public interface IntervalScheduleAccessor {
+        @Query("SELECT * FROM interval_schedule")
+        Result<IntervalSchedule> all();
+    }
+
+    public DAOIntervalSchedule(final MappingManager mappingManager) {
+        mapper = mappingManager.mapper(IntervalSchedule.class);
+        accessor = mappingManager.createAccessor(IntervalScheduleAccessor.class);
+    }
+
+    public Iterable<IntervalSchedule> all() {
+        return accessor.all();
+    }
+
+    public void save(final IntervalSchedule intervalSchedule) {
+        mapper.save(intervalSchedule);
+    }
+
+    public void delete(final IntervalSchedule intervalSchedule) {
+        mapper.delete(intervalSchedule);
+    }
+}
