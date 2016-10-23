11dabf3ce02698e10cbd6455aff8ff43f7925d00
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-10-23 10:41:16 -0400

    Fix mapper issue with scheduler.

--- v1x1-modules/v1x1-modules-core/v1x1-modules-core-scheduler/src/main/java/tv/v1x1/modules/core/scheduler/dto/IntervalSchedule.java
+++ v1x1-modules/v1x1-modules-core/v1x1-modules-core-scheduler/src/main/java/tv/v1x1/modules/core/scheduler/dto/IntervalSchedule.java
@@ -3,6 +3,7 @@ package tv.v1x1.modules.core.scheduler.dto;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 /**
@@ -12,14 +13,14 @@ import java.util.UUID;
 public class IntervalSchedule {
     @PartitionKey
     private UUID id;
-    private byte[] module;
-    private byte[] payload;
+    private ByteBuffer module;
+    private ByteBuffer payload;
     private long interval;
 
     public IntervalSchedule(final UUID id, final byte[] module, final byte[] payload, final long interval) {
         this.id = id;
-        this.module = module;
-        this.payload = payload;
+        this.module = ByteBuffer.wrap(module);
+        this.payload = ByteBuffer.wrap(payload);
         this.interval = interval;
     }
 
@@ -28,11 +29,19 @@ public class IntervalSchedule {
     }
 
     public byte[] getModule() {
-        return module;
+        byte[] bytes = new byte[module.remaining()];
+        module.mark();
+        module.get(bytes);
+        module.reset();
+        return bytes;
     }
 
     public byte[] getPayload() {
-        return payload;
+        byte[] bytes = new byte[payload.remaining()];
+        payload.mark();
+        payload.get(bytes);
+        payload.reset();
+        return bytes;
     }
 
     public long getInterval() {
