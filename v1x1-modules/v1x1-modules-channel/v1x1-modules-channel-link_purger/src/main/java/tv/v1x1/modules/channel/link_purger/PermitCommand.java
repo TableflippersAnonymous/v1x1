fcdab0c38382cb8897655db8f944267ff0430b91
Josh Johnson <SnoFox@SnoFox.net>
2016-10-24 00:23:13 -0700

    StateManager beginnings + Update LinkPurger

--- v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-link_purger/src/main/java/tv/v1x1/modules/channel/link_purger/PermitCommand.java
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-link_purger/src/main/java/tv/v1x1/modules/channel/link_purger/PermitCommand.java
@@ -1,8 +1,11 @@
 package tv.v1x1.modules.channel.link_purger;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import tv.v1x1.common.dto.core.Channel;
 import tv.v1x1.common.dto.core.ChatMessage;
 import tv.v1x1.common.dto.core.Permission;
+import tv.v1x1.common.services.chat.Chat;
 import tv.v1x1.common.util.commands.Command;
 
 import java.util.List;
@@ -48,6 +51,19 @@ public class PermitCommand extends Command {
 
     @Override
     public void run(ChatMessage chatMessage, String command, List<String> args) {
-        
+        final Channel channel = chatMessage.getChannel();
+        final String commanderName = chatMessage.getSender().getDisplayName();
+        final String targetId = module.getStateManager().getDisplayNameService().getIdFromDisplayName(channel, args.get(0));
+        if(targetId == null) {
+            Chat.message(module, channel, module.language.message(module.toDto(), "notarget",
+                    ImmutableMap.of("commander", commanderName, "target", args.get(0))));
+        }
+        if(module.permitUser(channel, targetId)) {
+            // TODO: final String targetName = module.getStateManager().getDisplayNameService().getDisplayNameFromId(channel, targetId);
+            Chat.message(module, channel, module.language.message(module.toDto(), "permit",
+                   ImmutableMap.of("target", targetId)));
+        } else {
+            Chat.message(module, channel, module.language.message(module.toDto(), "permitfailed", ImmutableMap.of("commander", commanderName)));
+        }
     }
 }
