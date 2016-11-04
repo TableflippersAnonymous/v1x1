230935f0dfe62646574a3cb9cb84e2b936f2cd59
Josh Johnson <SnoFox@SnoFox.net>
2016-11-03 17:12:16 -0700

    Update link_purger to leverage built-in permissions check + don't time out subs

--- v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-link_purger/src/main/java/tv/v1x1/modules/channel/link_purger/PermitCommand.java
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-link_purger/src/main/java/tv/v1x1/modules/channel/link_purger/PermitCommand.java
@@ -51,7 +51,6 @@ public class PermitCommand extends Command {
 
     @Override
     public void run(final ChatMessage chatMessage, final String command, final List<String> args) {
-        chatMessage.getPermissions().contains(getAllowedPermissions().get(0));
         final Channel channel = chatMessage.getChannel();
         final String commanderName = chatMessage.getSender().getDisplayName();
         final String targetId = module.getStateManager()
