8e8a38e65bfe4d069f9c8edee3e6ebdc81030fa3
Cobi <cobi@jacobicarter.com>
2016-11-06 22:29:09 -0500

    Fix ConfigScanner.

--- v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-timed_messages/src/main/java/tv/v1x1/modules/channel/timed_messages/TimedMessagesTenantConfiguration.java
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-timed_messages/src/main/java/tv/v1x1/modules/channel/timed_messages/TimedMessagesTenantConfiguration.java
@@ -1,13 +1,13 @@
 package tv.v1x1.modules.channel.timed_messages;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import tv.v1x1.common.config.ComplexType;
 import tv.v1x1.common.config.ConfigType;
 import tv.v1x1.common.config.Description;
 import tv.v1x1.common.config.DisplayName;
 import tv.v1x1.common.config.ModuleConfig;
 import tv.v1x1.common.config.Type;
 import tv.v1x1.common.modules.BasicTenantConfiguration;
-import tv.v1x1.common.modules.TenantConfiguration;
 
 import java.util.Map;
 
@@ -20,7 +20,8 @@ import java.util.Map;
 public class TimedMessagesTenantConfiguration extends BasicTenantConfiguration {
     @DisplayName("Timers")
     @Description("List of rotations you have configured")
-    @Type(ConfigType.COMPLEX_LIST)
+    @Type(ConfigType.COMPLEX_STRING_MAP)
+    @ComplexType(Timer.class)
     @JsonProperty("timers")
     private Map<String, Timer> timers;
 
