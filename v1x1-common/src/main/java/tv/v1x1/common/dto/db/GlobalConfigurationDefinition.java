b7087e0495a3775198b465fb15c2f318e1c7c2b6
Cobi <cobi@jacobicarter.com>
2016-11-06 22:09:38 -0500

    Add Complex types, also support ChannelConfiguration

--- v1x1-common/src/main/java/tv/v1x1/common/dto/db/GlobalConfigurationDefinition.java
+++ v1x1-common/src/main/java/tv/v1x1/common/dto/db/GlobalConfigurationDefinition.java
@@ -4,6 +4,7 @@ import com.datastax.driver.mapping.annotations.Table;
 import tv.v1x1.common.config.Permission;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -14,12 +15,19 @@ public class GlobalConfigurationDefinition extends ConfigurationDefinition {
     public GlobalConfigurationDefinition() {
     }
 
-    public GlobalConfigurationDefinition(final String name, final String displayName, final String description, final int version, final Permission tenantPermission, final List<Field> fields) {
-        super(name, displayName, description, version, tenantPermission, fields);
+    public GlobalConfigurationDefinition(final String name, final String displayName, final String description,
+                                         final int version, final Permission tenantPermission, final List<Field> fields,
+                                         final Map<String, List<Field>> complexFields) {
+        super(name, displayName, description, version, tenantPermission, fields, complexFields);
     }
 
     @Override
     public tv.v1x1.common.dto.core.GlobalConfigurationDefinition toCore() {
-        return new tv.v1x1.common.dto.core.GlobalConfigurationDefinition(getName(), getDisplayName(), getDescription(), getVersion(), getTenantPermission(), getFields().stream().map(Field::toCore).collect(Collectors.toList()));
+        return new tv.v1x1.common.dto.core.GlobalConfigurationDefinition(getName(), getDisplayName(), getDescription(),
+                getVersion(), getTenantPermission(), getFields().stream().map(Field::toCore).collect(Collectors.toList()),
+                getComplexFields().entrySet().stream().collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> e.getValue().stream().map(Field::toCore).collect(Collectors.toList())
+                )));
     }
 }
