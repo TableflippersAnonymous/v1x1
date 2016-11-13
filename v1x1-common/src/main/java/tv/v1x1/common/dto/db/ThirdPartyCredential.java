e6073fa1575d84ff044f6d5d6517f6ca46df51a8
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-11-13 18:28:31 -0500

    Hate ByteBuffer

--- v1x1-common/src/main/java/tv/v1x1/common/dto/db/ThirdPartyCredential.java
+++ v1x1-common/src/main/java/tv/v1x1/common/dto/db/ThirdPartyCredential.java
@@ -3,6 +3,8 @@ package tv.v1x1.common.dto.db;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by naomi on 11/12/2016.
  */
@@ -10,29 +12,25 @@ import com.datastax.driver.mapping.annotations.Table;
 public class ThirdPartyCredential {
     @PartitionKey
     private String name;
-    private byte[] credential;
+    private ByteBuffer credential;
 
     public ThirdPartyCredential() {
     }
 
     public ThirdPartyCredential(final String name, final byte[] credential) {
         this.name = name;
-        this.credential = credential;
+        this.credential = ByteBuffer.wrap(credential);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public byte[] getCredential() {
-        return credential;
-    }
-
-    public void setCredential(final byte[] credential) {
-        this.credential = credential;
+        final byte[] ret = new byte[credential.remaining()];
+        credential.mark();
+        credential.get(ret);
+        credential.reset();
+        return ret;
     }
 }
