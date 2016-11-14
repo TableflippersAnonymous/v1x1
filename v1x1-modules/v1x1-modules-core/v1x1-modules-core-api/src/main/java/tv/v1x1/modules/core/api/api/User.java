029ba867e326c683b38136ac80d983cf275d7fa9
Jacobi Carter <cobi@jacobicarter.com>
2016-11-13 22:58:20 -0500

    WIP

--- v1x1-modules/v1x1-modules-core/v1x1-modules-core-api/src/main/java/tv/v1x1/modules/core/api/api/User.java
+++ v1x1-modules/v1x1-modules-core/v1x1-modules-core-api/src/main/java/tv/v1x1/modules/core/api/api/User.java
@@ -1,7 +1,62 @@
 package tv.v1x1.modules.core.api.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import tv.v1x1.common.dto.db.Platform;
+
+import java.util.UUID;
+
 /**
  * Created by cobi on 10/26/2016.
  */
 public class User {
+    @JsonProperty("global_user_id")
+    private UUID globalUserId;
+    @JsonProperty
+    private Platform platform;
+    @JsonProperty("user_id")
+    private String userId;
+    @JsonProperty("display_name")
+    private String displayName;
+
+    public User() {
+    }
+
+    public User(final UUID globalUserId, final Platform platform, final String userId, final String displayName) {
+        this.globalUserId = globalUserId;
+        this.platform = platform;
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public UUID getGlobalUserId() {
+        return globalUserId;
+    }
+
+    public void setGlobalUserId(final UUID globalUserId) {
+        this.globalUserId = globalUserId;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(final Platform platform) {
+        this.platform = platform;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(final String userId) {
+        this.userId = userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(final String displayName) {
+        this.displayName = displayName;
+    }
 }
