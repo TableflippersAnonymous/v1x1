9dae8e867ca279585f21c2bba6faba1382bd0ea9
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-11-27 15:41:56 -0800

    Fix CD

--- codedeploy/prepare.sh
+++ codedeploy/prepare.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VERSION=$(git rev-parse --short HEAD)
-echo -n "$VERSION" > "${DIR}/version.txt"
+echo -n "$VERSION" > "${DIR}/v1x1-version.txt"
