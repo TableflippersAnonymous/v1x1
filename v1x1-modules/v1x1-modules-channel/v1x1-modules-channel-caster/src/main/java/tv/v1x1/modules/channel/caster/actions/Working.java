841dfb105681898482fa853236334a263dcdf69e
Josh Johnson <SnoFox@SnoFox.net>
2016-12-09 19:15:45 -0800

    Squashed commit of the following:
    commit 27828006469823673345162401fb2f4a7840b3ee
    Author: Josh Johnson <SnoFox@SnoFox.net>
    Date:   Fri Dec 9 19:13:54 2016 -0800
    
        Add remaining creative communities
    
    commit 0ae1ddfccbd9b7e0410b340f94c050cc721977ca
    Author: Josh Johnson <SnoFox@SnoFox.net>
    Date:   Fri Dec 9 17:10:39 2016 -0800
    
        Support more creative communities
    
    commit 4f055f0c4b7764ff8331e0a1c52262ef30cfa054
    Author: Josh Johnson <SnoFox@SnoFox.net>
    Date:   Thu Dec 8 13:07:20 2016 -0800
    
        Caster module

---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-caster/src/main/java/tv/v1x1/modules/channel/caster/actions/Working.java
@@ -0,0 +1,10 @@
+package tv.v1x1.modules.channel.caster.actions;
+
+/**
+ * @author Josh
+ */
+public class Working extends GenericAction {
+    public Working(final String material) {
+        action = "working with " + material;
+    }
+}
