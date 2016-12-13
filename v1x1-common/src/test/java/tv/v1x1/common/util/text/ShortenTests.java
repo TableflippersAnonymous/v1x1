621b15a888c3d9252f632897d1972e584507ac02
Naomi Persephone Amethyst <naomi@amethyst.name>
2016-12-13 01:06:28 -0500

    Add basic tests.

---
+++ v1x1-common/src/test/java/tv/v1x1/common/util/text/ShortenTests.java
@@ -0,0 +1,33 @@
+package tv.v1x1.common.util.text;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by naomi on 12/13/2016.
+ */
+public class ShortenTests {
+    @Test
+    public void testGenPreviewShort() {
+        Assert.assertEquals(Shorten.genPreview("a", 10), "a");
+    }
+
+    @Test
+    public void testGenPreviewMedium() {
+        Assert.assertEquals(Shorten.genPreview("1234567890", 10), "1234567890");
+    }
+
+    @Test
+    public void testGenPreviewLong() {
+        Assert.assertEquals(Shorten.genPreview("12345678901234567890", 10), "1234567890...");
+    }
+
+    @Test
+    public void testGenPreviewWordBoundary() {
+        Assert.assertEquals(Shorten.genPreview("This is just a test.", 10), "This is just...");
+    }
+    @Test
+    public void testGenPreviewWords() {
+        Assert.assertEquals(Shorten.genPreview("If we test, then we test", 9), "If we test,...");
+    }
+}
