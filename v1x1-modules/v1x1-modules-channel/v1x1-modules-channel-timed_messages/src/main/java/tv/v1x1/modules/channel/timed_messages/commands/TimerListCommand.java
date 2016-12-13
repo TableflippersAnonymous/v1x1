391f43630a29b12c49ce483574fe943e8d6c3836
Josh Johnson <SnoFox@SnoFox.net>
2016-12-13 00:12:40 -0800

    More visibility into timers

---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-timed_messages/src/main/java/tv/v1x1/modules/channel/timed_messages/commands/TimerListCommand.java
@@ -0,0 +1,55 @@
+package tv.v1x1.modules.channel.timed_messages.commands;
+
+import com.google.common.collect.ImmutableList;
+import tv.v1x1.common.dto.core.Channel;
+import tv.v1x1.common.dto.core.ChatMessage;
+import tv.v1x1.common.dto.core.Permission;
+import tv.v1x1.common.services.chat.Chat;
+import tv.v1x1.common.util.commands.Command;
+import tv.v1x1.common.util.text.Shorten;
+import tv.v1x1.modules.channel.timed_messages.TimedMessages;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Josh
+ */
+/* pkg-private */ class TimerListCommand extends Command {
+    private TimedMessages module;
+
+    TimerListCommand(final TimedMessages module) {
+        this.module = module;
+    }
+
+    @Override
+    public List<String> getCommands() {
+        return ImmutableList.of("list");
+    }
+
+    @Override
+    public List<Permission> getAllowedPermissions() {
+        return null;
+    }
+
+    @Override
+    public void run(final ChatMessage chatMessage, final String command, final List<String> args) {
+        final Channel channel = chatMessage.getChannel();
+        final String senderName = chatMessage.getSender().getDisplayName();
+        final Set<String> timers = module.listTimers(channel.getTenant());
+        if(timers.size() < 1) {
+            Chat.i18nMessage(module, channel, "list.empty",
+                    "commander", senderName);
+        } else {
+            Chat.i18nMessage(module, channel, "list.success",
+                    "commander", senderName,
+                    "timers", String.join(", ", timers)
+            );
+        }
+    }
+
+    @Override
+    public String getUsage() {
+        return "list";
+    }
+}
