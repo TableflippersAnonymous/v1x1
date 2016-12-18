a237a8cef8f1d677ad0be749069612e197e11ec7
Josh Johnson <SnoFox@SnoFox.net>
2016-12-18 09:35:39 -0800

    Fixup 79c6054

---
+++ v1x1-common/src/main/java/tv/v1x1/common/util/validation/TwitchValidator.java
@@ -0,0 +1,13 @@
+package tv.v1x1.common.util.validation;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Josh
+ */
+public class TwitchValidator {
+    private static Pattern USERNAME_REGEX = Pattern.compile("^[a-zA-Z0-9_]{4,25}$");
+    public static boolean isValidUsername(final String username) {
+        return USERNAME_REGEX.matcher(username).matches();
+    }
+}
