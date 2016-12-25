0e753a0668f136150906376643c94fb8d8bee55a
Josh Johnson <SnoFox@SnoFox.net>
2016-12-24 20:48:24 -0800

    +ops tool

---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-ops_tool/src/main/java/tv/v1x1/modules/channel/ops_tool/commands/permissions/GroupInfoCommand.java
@@ -0,0 +1,83 @@
+package tv.v1x1.modules.channel.ops_tool.commands.permissions;
+
+import com.google.common.collect.ImmutableList;
+import tv.v1x1.common.dto.core.Channel;
+import tv.v1x1.common.dto.core.ChatMessage;
+import tv.v1x1.common.dto.core.Permission;
+import tv.v1x1.common.dto.core.Tenant;
+import tv.v1x1.common.dto.db.TenantGroup;
+import tv.v1x1.common.util.commands.Command;
+import tv.v1x1.modules.channel.ops_tool.OpsTool;
+
+import java.util.List;
+
+/**
+ * @author Josh
+ */
+public class GroupInfoCommand extends Command {
+    final private OpsTool opsTool;
+
+    public GroupInfoCommand(final OpsTool opsTool) {
+        this.opsTool = opsTool;
+    }
+
+    @Override
+    public List<String> getCommands() {
+        return ImmutableList.of("info");
+    }
+
+    @Override
+    public String getDescription() {
+        return "create a group for tenant";
+    }
+
+    @Override
+    public List<Permission> getAllowedPermissions() {
+        return null;
+    }
+
+    @Override
+    public String getUsage() {
+        return "<channel> <vgroup>";
+    }
+
+    @Override
+    public int getMinArgs() {
+        return 2;
+    }
+
+    @Override
+    public void run(final ChatMessage chatMessage, final String command, final List<String> args) {
+        final Tenant tenant;
+        final String search = args.get(0);
+        final String groupName = args.get(1);
+        final Channel channel = chatMessage.getChannel();
+        tenant = opsTool.getTenantByChannel(chatMessage.getChannel().getPlatform(), search);
+        if(tenant == null) {
+            opsTool.respond(channel, "I don't know about that channel");
+            return;
+        }
+        TenantGroup group = opsTool.groupByName(tenant, groupName);
+        if(group == null) {
+            opsTool.respond(channel, "Group doesn't exist");
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: ");
+        sb.append(group.getName());
+        sb.append(" | UUID: ");
+        sb.append(group.getGroupId());
+        sb.append(" | Perms: ");
+        if(group.getPermissions() != null) {
+            boolean first = true;
+            for(tv.v1x1.common.dto.db.Permission perm : group.getPermissions()) {
+                if(first) first = false;
+                else sb.append(", ");
+                sb.append(perm.toCore());
+            }
+        } else {
+            sb.append("<none>");
+        }
+        opsTool.respond(channel, sb.toString());
+    }
+}
---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-ops_tool/src/main/java/tv/v1x1/modules/channel/ops_tool/commands/permissions/GroupListCommand.java
@@ -0,0 +1,67 @@
+package tv.v1x1.modules.channel.ops_tool.commands.permissions;
+
+import com.google.common.collect.ImmutableList;
+import tv.v1x1.common.dto.core.Channel;
+import tv.v1x1.common.dto.core.ChatMessage;
+import tv.v1x1.common.dto.core.Permission;
+import tv.v1x1.common.dto.core.Tenant;
+import tv.v1x1.common.dto.db.TenantGroup;
+import tv.v1x1.common.util.commands.Command;
+import tv.v1x1.modules.channel.ops_tool.OpsTool;
+
+import java.util.List;
+
+/**
+ * @author Josh
+ */
+public class GroupListCommand extends Command {
+    final private OpsTool opsTool;
+
+    public GroupListCommand(final OpsTool opsTool) {
+        this.opsTool = opsTool;
+    }
+
+    @Override
+    public List<String> getCommands() {
+        return ImmutableList.of("list");
+    }
+
+    @Override
+    public String getDescription() {
+        return "list groups for tenant";
+    }
+
+    @Override
+    public List<Permission> getAllowedPermissions() {
+        return null;
+    }
+
+    @Override
+    public String getUsage() {
+        return "<channel>";
+    }
+
+    @Override
+    public int getMinArgs() {
+        return 1;
+    }
+
+    @Override
+    public void run(final ChatMessage chatMessage, final String command, final List<String> args) {
+        final Tenant tenant;
+        final String search = args.get(0);
+        final Channel channel = chatMessage.getChannel();
+        tenant = opsTool.getTenantByChannel(chatMessage.getChannel().getPlatform(), search);
+        if(tenant == null) {
+            opsTool.respond(channel, "I don't know about that channel");
+        }
+        final Iterable<TenantGroup> groups = opsTool.getGroups(tenant);
+        final StringBuilder sb = new StringBuilder();
+        sb.append("List of groups: ");
+        for(TenantGroup group : groups) {
+            sb.append(group.getName());
+            sb.append(' ');
+        }
+        opsTool.respond(channel, sb.toString());
+    }
+}
