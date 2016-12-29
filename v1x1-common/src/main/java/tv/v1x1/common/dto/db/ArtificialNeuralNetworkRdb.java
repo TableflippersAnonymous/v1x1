4ed6d4f7cb4e394581b82222c3e7e755d3a4335e
Cobi <cobi@jacobicarter.com>
2016-12-29 18:44:35 -0500

    Beginnings of machinelearning

---
+++ v1x1-common/src/main/java/tv/v1x1/common/dto/db/ArtificialNeuralNetworkRdb.java
@@ -0,0 +1,7 @@
+package tv.v1x1.common.dto.db;
+
+/**
+ * Created by cobi on 12/29/2016.
+ */
+public class ArtificialNeuralNetworkRdb {
+}
---
+++ v1x1-common/src/main/java/tv/v1x1/common/services/machinelearning/ArtificialNeuralNetwork.java
@@ -0,0 +1,10 @@
+package tv.v1x1.common.services.machinelearning;
+
+/**
+ * Created by cobi on 12/29/2016.
+ */
+public class ArtificialNeuralNetwork {
+    public ArtificialNeuralNetwork() {
+
+    }
+}
