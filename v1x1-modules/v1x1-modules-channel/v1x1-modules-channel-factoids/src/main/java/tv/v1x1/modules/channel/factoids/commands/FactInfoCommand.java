8c2ff9e8ee42359ed53ad632bf6101c03fc7ef7f
Josh Johnson <SnoFox@SnoFox.net>
2017-01-09 18:57:43 -0800

    Factoids complete

---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-factoids/src/main/java/tv/v1x1/modules/channel/factoids/commands/FactInfoCommand.java
@@ -0,0 +1,86 @@
+package tv.v1x1.modules.channel.factoids.commands;
+
+import com.google.common.collect.ImmutableList;
+import tv.v1x1.common.dto.core.Channel;
+import tv.v1x1.common.dto.core.ChatMessage;
+import tv.v1x1.common.dto.core.Permission;
+import tv.v1x1.common.services.chat.Chat;
+import tv.v1x1.common.util.commands.Command;
+import tv.v1x1.common.util.text.Shorten;
+import tv.v1x1.modules.channel.factoids.FactoidsModule;
+import tv.v1x1.modules.channel.factoids.dao.Factoid;
+
+import java.util.List;
+
+/**
+ * @author Josh
+ */
+public class FactInfoCommand extends Command {
+    final private FactoidsModule module;
+
+    public FactInfoCommand(FactoidsModule module) {
+        this.module = module;
+    }
+
+    @Override
+    public List<String> getCommands() {
+        return ImmutableList.of("info");
+    }
+
+    @Override
+    public void run(final ChatMessage chatMessage, final String command, final List<String> args) {
+        final Channel channel = chatMessage.getChannel();
+        final String commander = chatMessage.getSender().getDisplayName();
+        final String factName = args.get(0).toLowerCase();
+        final Factoid fact = module.getFactDirectly(channel.getTenant(), factName);
+        if(fact == null) {
+            Chat.i18nMessage(module, channel, "noexist",
+            "commander", commander,
+                    "id", factName);
+        } else {
+            if(fact.isAlias()) {
+                Chat.i18nMessage(module, channel, "info.alias",
+                        "commander", commander,
+                        "id", fact.getId(),
+                        "to", fact.getData());
+            } else {
+                if(fact.getPermission() == null) {
+                    Chat.i18nMessage(module, channel, "info.noperm",
+                            "commander", commander,
+                            "id", fact.getId(),
+                            "fact", Shorten.genPreview(fact.getData(), 100));
+                } else {
+                    Chat.i18nMessage(module, channel, "info.standard",
+                            "commander", commander,
+                            "id", fact.getId(),
+                            "perm", fact.getPermission().getNode(),
+                            "fact", Shorten.genPreview(fact.getData(), 100));
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getUsage() {
+        return "<fact>";
+    }
+
+    @Override
+    public String getDescription() {
+        return "get information about a fact";
+    }
+
+    @Override
+    public int getMinArgs() {
+        return 1;
+    }
+
+    @Override
+    public void handleArgMismatch(final ChatMessage chatMessage, final String command, final List<String> args) {
+        if(args.size() < 1) {
+            Chat.i18nMessage(module, chatMessage.getChannel(), "invalid.args",
+                    "commander", chatMessage.getSender().getDisplayName(),
+                    "usage", getUsage());
+        }
+    }
+}
