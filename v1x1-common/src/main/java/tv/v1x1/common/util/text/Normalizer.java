22ca2fca79838aa04b47f35156769206cfc90ddb
Cobi <cobi@jacobicarter.com>
2017-01-11 00:39:03 -0500

    Normalizer skeleton; bump conn limit

---
+++ v1x1-common/src/main/java/tv/v1x1/common/util/text/Normalizer.java
@@ -0,0 +1,11 @@
+package tv.v1x1.common.util.text;
+
+/**
+ * Created by cobi on 1/10/2017.
+ */
+public class Normalizer {
+
+    public static String normalize(final String input) {
+        return input;
+    }
+}
