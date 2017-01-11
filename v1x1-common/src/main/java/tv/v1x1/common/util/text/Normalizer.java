1248df8f4de0a309c4abce608eea7812128291b5
Naomi Persephone Amethyst <naomi@amethyst.name>
2017-01-11 00:39:03 -0500

    Normalizer skeleton; bump conn limit

---
+++ v1x1-common/src/main/java/tv/v1x1/common/util/text/Normalizer.java
@@ -0,0 +1,11 @@
+package tv.v1x1.common.util.text;
+
+/**
+ * Created by naomi on 1/10/2017.
+ */
+public class Normalizer {
+
+    public static String normalize(final String input) {
+        return input;
+    }
+}
--- v1x1-web/app/app.js
+++ v1x1-web/app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('v1x1', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'v1x1.view1',
+  'v1x1.view2',
+  'v1x1.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
