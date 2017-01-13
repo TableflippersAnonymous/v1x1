67eb9d24454da7355fd03239bf3ad849eed1c8e8
Naomi Persephone Amethyst <naomi@amethyst.name>
2017-01-13 02:19:41 -0500

    More UI stuffs.

--- v1x1-web/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
-})
-export class AppComponent  { name = 'v1x1'; }
---
+++ v1x1-web/app/components/config.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'config',
+  template: `<ngb-tab></ngb-tab>`
+})
+export class ConfigComponent { }
