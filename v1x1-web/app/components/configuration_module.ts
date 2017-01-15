f1a703b89b292efba4fc527ae0f42d82c81e6e85
Naomi Persephone Amethyst <naomi@amethyst.name>
2017-01-15 08:05:45 -0500

    More v1x1 web.

--- v1x1-web/app/components/configuration_module.ts
+++ v1x1-web/app/components/configuration_module.ts
@@ -3,7 +3,28 @@ import {V1x1Module} from "../model/v1x1_module";
 
 @Component({
   selector: 'configuration-module',
-  template: `<p>{{v1x1Module.name}} Configuration stuffs</p>`
+  template: `
+<b>{{v1x1Module.displayName}}</b>
+<p>Description: {{v1x1Module.description}}</p>
+<hr>
+<ngb-tabset>
+  <ngb-tab title="Global" *ngIf="v1x1Module.configurationDefinitionSet.global != null">
+    <template ngbTabContent>
+      <configuration-scope [v1x1Module]="v1x1Module" [configurationDefinition]="v1x1Module.configurationDefinitionSet.global" [configuration]="{}" [id]="null" [scope]="'global'"></configuration-scope>
+    </template>
+  </ngb-tab>
+  <ngb-tab title="Tenant" *ngIf="v1x1Module.configurationDefinitionSet.tenant != null">
+    <template ngbTabContent>
+      <configuration-scope [v1x1Module]="v1x1Module" [configurationDefinition]="v1x1Module.configurationDefinitionSet.tenant" [configuration]="{}" [id]="null" [scope]="'tenant'"></configuration-scope>
+    </template>
+  </ngb-tab>
+  <ngb-tab title="Channel" *ngIf="v1x1Module.configurationDefinitionSet.channel != null">
+    <template ngbTabContent>
+      <configuration-scope [v1x1Module]="v1x1Module" [configurationDefinition]="v1x1Module.configurationDefinitionSet.channel" [configuration]="{}" [id]="null" [scope]="'channel'"></configuration-scope>
+    </template>
+  </ngb-tab>
+</ngb-tabset>
+`
 })
 export class ConfigurationModuleComponent {
   @Input() public v1x1Module: V1x1Module;
--- v1x1-web/app/model/v1x1_module.ts
+++ v1x1-web/app/model/v1x1_module.ts
@@ -1,9 +1,14 @@
 import {V1x1ConfigurationDefinitionSet} from "./v1x1_configuration_definition_set";
 export class V1x1Module {
   name: string;
+  displayName: string;
+  description: string;
   configurationDefinitionSet: V1x1ConfigurationDefinitionSet;
 
-  constructor(name: string) {
+  constructor(name: string, displayName: string, description: string, configurationDefinitionSet: V1x1ConfigurationDefinitionSet) {
     this.name = name;
+    this.displayName = displayName;
+    this.description = description;
+    this.configurationDefinitionSet = configurationDefinitionSet;
   }
 }
