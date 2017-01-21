3e2349c13e251c09beb71591e0274e1d73060e61
Jacobi Carter <cobi@jacobicarter.com>
2017-01-21 04:30:40 -0500

    Bettar Caches (Yay Redis!)

--- v1x1-common/src/main/java/tv/v1x1/common/services/cache/SharedCache.java
+++ v1x1-common/src/main/java/tv/v1x1/common/services/cache/SharedCache.java
@@ -1,7 +1,13 @@
 package tv.v1x1.common.services.cache;
 
+import java.io.Closeable;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by cobi on 10/5/16.
  */
-public interface SharedCache {
+public interface SharedCache<V> extends Closeable {
+    V get(final byte[] key) throws ExecutionException;
+    void put(final byte[] key, final V value);
+    void invalidate(final byte[] key);
 }
--- v1x1-common/src/main/java/tv/v1x1/common/services/persistence/TenantConfigurationProvider.java
+++ v1x1-common/src/main/java/tv/v1x1/common/services/persistence/TenantConfigurationProvider.java
@@ -6,10 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
 import tv.v1x1.common.dao.DAOTenantConfiguration;
 import tv.v1x1.common.dto.core.Module;
 import tv.v1x1.common.dto.core.Tenant;
+import tv.v1x1.common.dto.core.UUID;
+import tv.v1x1.common.dto.proto.core.ChannelOuterClass;
+import tv.v1x1.common.dto.proto.core.UUIDOuterClass;
 import tv.v1x1.common.modules.TenantConfiguration;
+import tv.v1x1.common.services.cache.CacheManager;
+import tv.v1x1.common.services.cache.RedisCache;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -18,25 +24,34 @@ import java.util.concurrent.TimeUnit;
  * Created by cobi on 10/17/2016.
  */
 public class TenantConfigurationProvider<T extends TenantConfiguration> {
-    private final LoadingCache<Tenant, T> cache;
+    private final LoadingCache<byte[], T> cache;
+    private final RedisCache sharedCache;
     private final DAOTenantConfiguration daoTenantConfiguration;
     private final ObjectMapper mapper;
     private final Module module;
 
-    public TenantConfigurationProvider(final Module module, final DAOManager daoManager, final Class<T> clazz) {
+    public TenantConfigurationProvider(final Module module, final CacheManager cacheManager, final DAOManager daoManager, final Class<T> clazz) {
         daoTenantConfiguration = daoManager.getDaoTenantConfiguration();
         mapper = new ObjectMapper(new JsonFactory());
         this.module = module;
+
+        sharedCache = cacheManager.redisCache("TenantConfigurationProvider|" + module.getName(), 10, TimeUnit.MINUTES, new CacheLoader<byte[], byte[]>() {
+            @Override
+            public byte[] load(final byte[] tenantData) throws Exception {
+                final tv.v1x1.common.dto.db.TenantConfiguration tenantConfiguration = daoTenantConfiguration.get(module, Tenant.fromProto(ChannelOuterClass.Tenant.parseFrom(tenantData)));
+                if(tenantConfiguration == null)
+                    return "{}".getBytes();
+                return tenantConfiguration.getJson().getBytes();
+            }
+        });
+
         cache = CacheBuilder.newBuilder()
                 .expireAfterWrite(10, TimeUnit.SECONDS)
-                .build(new CacheLoader<Tenant, T>() {
+                .build(new CacheLoader<byte[], T>() {
                     @Override
-                    public T load(final Tenant tenant) throws Exception {
+                    public T load(final byte[] key) throws Exception {
                         try {
-                            final tv.v1x1.common.dto.db.TenantConfiguration tenantConfiguration = daoTenantConfiguration.get(module, tenant);
-                            if (tenantConfiguration == null)
-                                return mapper.readValue("{}", clazz);
-                            return mapper.readValue(tenantConfiguration.getJson(), clazz);
+                            return mapper.readValue(new String(key), clazz);
                         } catch(final Exception e) {
                             e.printStackTrace();
                             throw e;
@@ -47,7 +62,7 @@ public class TenantConfigurationProvider<T extends TenantConfiguration> {
 
     public T getTenantConfiguration(final Tenant tenant) {
         try {
-            return cache.get(tenant);
+            return cache.get(sharedCache.get(tenant.toProto().toByteArray()));
         } catch (final ExecutionException e) {
             throw new RuntimeException(e);
         }
@@ -57,6 +72,7 @@ public class TenantConfigurationProvider<T extends TenantConfiguration> {
         try {
             final tv.v1x1.common.dto.db.TenantConfiguration dbConfiguration = new tv.v1x1.common.dto.db.TenantConfiguration(module.getName(), tenant.getId().getValue(), mapper.writeValueAsString(configuration));
             daoTenantConfiguration.put(dbConfiguration);
+            sharedCache.put(tenant.toProto().toByteArray(), dbConfiguration.getJson().getBytes());
         } catch (final JsonProcessingException e) {
             throw new RuntimeException(e);
         }
