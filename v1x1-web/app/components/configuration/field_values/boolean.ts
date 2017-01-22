4c037b169c893ec221caa57d9d9da235f3c196a6
Cobi <cobi@jacobicarter.com>
2017-01-22 01:21:29 -0500

    More structure, stubs for other nav pages.

--- v1x1-web/app/components/configuration/field_values/boolean.ts
+++ v1x1-web/app/components/configuration/field_values/boolean.ts
@@ -1,6 +1,6 @@
 import {Component, Input, Output, EventEmitter} from "@angular/core";
-import {V1x1ConfigurationDefinitionField} from "../model/v1x1_configuration_definition_field";
-import {ConfigurableComponent} from "./configurable";
+import {V1x1ConfigurationDefinitionField} from "../../../model/v1x1_configuration_definition_field";
+import {ConfigurableComponent} from "../configurable";
 @Component({
   selector: 'configuration-field-value-boolean',
   template: `<label class="form-check-label"><input type="checkbox" [(ngModel)]="configuration" class="form-check-input"> {{field.displayName}}</label>`
---
+++ v1x1-web/app/components/permissions/page.ts
@@ -0,0 +1,6 @@
+import {Component} from "@angular/core";
+@Component({
+  selector: 'permissions-page',
+  template: `Permissions page goes here`
+})
+export class PermissionsPageComponent {}
