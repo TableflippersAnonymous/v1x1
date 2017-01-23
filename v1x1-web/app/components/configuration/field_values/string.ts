e19f527c1911934e8b79e79df5b401a46e3fcea8
Cobi <cobi@jacobicarter.com>
2017-01-23 00:13:05 -0500

    More UI stuff

--- v1x1-web/app/components/configuration/field_values/string.ts
+++ v1x1-web/app/components/configuration/field_values/string.ts
@@ -1,10 +1,21 @@
-import {Component, Input, Output} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {V1x1ConfigurationDefinitionField} from "../../../model/v1x1_configuration_definition_field";
 import {ConfigurableComponent} from "../configurable";
 @Component({
   selector: 'configuration-field-value-string',
-  template: `<input type="text" [(ngModel)]="configuration" class="form-control" placeholder="{{field.defaultValue}}">`
+  template: `
+    <div class="input-group">
+      <input type="text" [ngModel]="configuration" (blur)="configuration = $event.target.value" class="form-control" placeholder="{{field.defaultValue}}">
+      <span class="input-group-btn" *ngIf="configDirty() && originalValid()">
+        <button class="btn btn-secondary" (click)="abandonChanges()"><i class="fa fa-undo"></i></button>
+      </span>
+    </div>
+  `
 })
 export class ConfigurationFieldValueStringComponent extends ConfigurableComponent {
   @Input() public field: V1x1ConfigurationDefinitionField;
+
+  originalValid() {
+    return typeof this.originalConfiguration === 'string';
+  }
 }
