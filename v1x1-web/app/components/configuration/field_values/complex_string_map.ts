b5aac352bef8768ef7e08588f4a1ed73d7d764cb
Naomi Persephone Amethyst <naomi@amethyst.name>
2017-01-23 00:13:05 -0500

    More UI stuff

--- v1x1-web/app/components/configuration/field_values/complex_string_map.ts
+++ v1x1-web/app/components/configuration/field_values/complex_string_map.ts
@@ -14,7 +14,7 @@ import {ConfigurableComponent} from "../configurable";
             </span>
           </div>
         </div>
-        <div class="col" style="border-left: 2px solid rgb(238, 238, 238); margin-bottom: 1rem;">
+        <div class="col config-group" style="margin-bottom: 1rem;" [class.config-group-dirty]="configFieldDirty(elem)">
           <configuration-field-value-complex [field]="field" [complexFields]="complexFields" [originalConfiguration]="originalConfiguration[elem]" [configuration]="configuration[elem]" (configurationChange)="setConfigField(elem, $event)"></configuration-field-value-complex>
         </div>
       </div>
@@ -35,9 +35,12 @@ export class ConfigurationFieldValueComplexStringMapComponent extends Configurab
   @Input() public field: V1x1ConfigurationDefinitionField;
   @Input() public complexFields: {[key: string]: V1x1ConfigurationDefinitionField[]};
 
-  public nextValue: string;
+  public nextValue: string = "";
 
   public addValue(value: string) {
+    if(this.configuration instanceof Object)
+      while(this.configuration.hasOwnProperty(value))
+        value = value + "_";
     this.setConfigField(value, {});
     this.nextValue = "";
   }
@@ -45,8 +48,10 @@ export class ConfigurationFieldValueComplexStringMapComponent extends Configurab
   public changeKey(oldKey: string, newKey: string) {
     if(oldKey === newKey)
       return;
-    this.setConfigField(newKey, this.configuration[oldKey]);
-    this.deleteConfigField(oldKey);
+    if(this.configuration instanceof Object)
+      while(this.configuration.hasOwnProperty(newKey))
+        newKey = newKey + "_";
+    this.renameConfigField(oldKey, newKey);
   }
 
   public delKey(key: string) {
