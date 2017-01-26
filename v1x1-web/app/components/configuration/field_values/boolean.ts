cbe5c0a40fc02bd85d462ef8b0c2a26920f137ac
Cobi <cobi@jacobicarter.com>
2017-01-26 05:48:33 -0500

    v1x1-web: WIP, lots.  Also webpack.

--- v1x1-web/app/components/configuration/field_values/boolean.ts
+++ v1x1-web/app/components/configuration/field_values/boolean.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {V1x1ConfigurationDefinitionField} from "../../../model/v1x1_configuration_definition_field";
 import {ConfigurableComponent} from "../configurable";
 @Component({
---
+++ v1x1-web/config/webpack.dev.js
@@ -0,0 +1,35 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:3000/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+      '/bootstrap.min.css': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }
+  }
+
+});
