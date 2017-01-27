ef9489a7831a12ec8ca6042395a22c33f34d37d5
Jacobi Carter <cobi@jacobicarter.com>
2017-01-26 21:39:25 -0800

    Make builds faster?

---
+++ Makefile
@@ -0,0 +1,36 @@
+dependencies: mvn-dep npm-dep pip-dep docker-dep
+	aws configure set preview.cloudfront true
+
+mvn-dep:
+	mvn --fail-never dependency:go-offline || true
+
+npm-dep:
+	(cd v1x1-web && npm install)
+
+pip-dep:
+	pip install awscli
+
+docker-dep:
+	docker pull openjdk:8-jre-alpine
+
+test: mvn-test npm-build cert-setup docker-login
+	./upload.sh
+	codedeploy/prepare.sh
+
+mvn-test:
+	mvn -T8 integration-test -Denvironment=prod
+
+npm-build:
+	(cd v1x1-web && npm run build)
+
+cert-setup:
+	sudo mkdir -p /etc/docker/certs.d/registry.tblflp.zone:5443
+	sudo wget -O /etc/docker/certs.d/registry.tblflp.zone:5443/ca.crt http://pki.tblflp.zone/Tableflippers-Anonymous-Root-CA.pem
+	cat /etc/ssl/certs/ca-certificates.crt | sudo tee -a /etc/docker/certs.d/registry.tblflp.zone:5443/ca.crt
+
+docker-login:
+	docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS registry.tblflp.zone:5443
+	echo '#!/bin/bash' > codedeploy/v1x1-login.sh
+	echo "docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS registry.tblflp.zone:5443" >> codedeploy/v1x1-login.sh
+	chmod +x codedeploy/v1x1-login.sh
+
--- upload.sh
+++ upload.sh
@@ -12,24 +12,42 @@ echo "Found $MODULECOUNT"
 echo -n "Getting current revision... "
 VERSION=$(git rev-parse --short HEAD)
 echo $VERSION
-echo "Uploading..."
+echo "Building plan ..."
+echo > Makefile.upload
+MAKEFILE_TARGETS=""
 for MODULE in $MODULES; do
-    docker tag ${MODULE}:latest ${REGISTRY}/${MODULE}:${VERSION}
-    docker push ${REGISTRY}/${MODULE}:${VERSION}
+    MAKEFILE_TARGETS="${MAKEFILE_TARGETS} ${MODULE}"
+    echo "${MODULE}:" >> Makefile.upload
+    echo -e "\tdocker tag ${MODULE}:latest ${REGISTRY}/${MODULE}:${VERSION}" >> Makefile.upload
+    echo -e "\tdocker push ${REGISTRY}/${MODULE}:${VERSION}" >> Makefile.upload
 done
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
-    (
-        cd v1x1-web/dist
-        for file in *.css ; do
-            aws s3 cp --content-type text/css "${file}" s3://v1x1-web/
-        done
-        for file in *.js ; do
-            aws s3 cp --content-type application/javascript "${file}" s3://v1x1-web/
-        done
-        for file in *.html ; do
-            aws s3 cp --content-type text/html --cache-control max-age=3600 "${file}" s3://v1x1-web/
-        done
-        aws cloudfront create-invalidation --distribution-id E1ES20CHGVRXZB --paths '/index.html' '/*.html'
-    )
+    for file in v1x1-web/dist/*.css ; do
+        MAKEFILE_TARGETS="${MAKEFILE_TARGETS} ${file}"
+        echo "${file}:" >> Makefile.upload
+        echo -e "\taws s3 cp --content-type text/css ${file} s3://v1x1-web/" >> Makefile.upload
+    done
+    for file in v1x1-web/dist/*.js ; do
+        MAKEFILE_TARGETS="${MAKEFILE_TARGETS} ${file}"
+        echo "${file}:" >> Makefile.upload
+        echo -e "\taws s3 cp --content-type application/javascript ${file} s3://v1x1-web/" >> Makefile.upload
+    done
+    for file in v1x1-web/dist/*.html ; do
+        MAKEFILE_TARGETS="${MAKEFILE_TARGETS} ${file}"
+        echo "${file}:" >> Makefile.upload
+        echo -e "\taws s3 cp --content-type text/html --cache-control max-age=3600 ${file} s3://v1x1-web/" >> Makefile.upload
+    done
+fi
+
+echo "upload: ${MAKEFILE_TARGETS}" >> Makefile.upload
+
+echo "Uploading..."
+make -j -f Makefile.upload upload
+
+rm -f Makefile.upload
+
+if [[ $CIRCLE_BRANCH == "master" ]]; then
+    echo "Invalidating Cloudfront..."
+    aws cloudfront create-invalidation --distribution-id E1ES20CHGVRXZB --paths '/index.html' '/*.html'
 fi
