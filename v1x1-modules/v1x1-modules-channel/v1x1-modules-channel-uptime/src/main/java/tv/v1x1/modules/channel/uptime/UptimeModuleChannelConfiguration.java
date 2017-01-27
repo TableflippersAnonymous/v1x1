8e61a2715cd5caeabd5ccf7111aef0d0cb71f35d
Josh Johnson <SnoFox@SnoFox.net>
2017-01-26 20:55:42 -0800

    Uptime module

---
+++ v1x1-modules/v1x1-modules-channel/v1x1-modules-channel-uptime/src/main/java/tv/v1x1/modules/channel/uptime/UptimeModuleChannelConfiguration.java
@@ -0,0 +1,9 @@
+package tv.v1x1.modules.channel.uptime;
+
+import tv.v1x1.common.modules.BasicChannelConfiguration;
+
+/**
+ * @author Josh
+ */
+public class UptimeModuleChannelConfiguration extends BasicChannelConfiguration {
+}
