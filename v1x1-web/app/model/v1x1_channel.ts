092045e7d7ee7128c73f9d642f0ab73aaee2b66b
Jacobi Carter <cobi@jacobicarter.com>
2017-02-03 09:27:23 -0500

    Tenant selector; also pull configs from API

--- v1x1-web/app/model/v1x1_channel.ts
+++ v1x1-web/app/model/v1x1_channel.ts
@@ -1,3 +1,12 @@
+import {JsonObject, JsonProperty} from "json2typescript";
+@JsonObject
 export class V1x1Channel {
-
+  @JsonProperty("tenant_id")
+  tenantId: string = undefined;
+  @JsonProperty("platform")
+  platform: string = undefined;
+  @JsonProperty("display_name")
+  displayName: string = undefined;
+  @JsonProperty("channel_id")
+  channelId: string = undefined;
 }
--- v1x1-web/app/model/v1x1_module.ts
+++ v1x1-web/app/model/v1x1_module.ts
@@ -1,22 +1,21 @@
 import {V1x1ConfigurationDefinitionSet} from "./v1x1_configuration_definition_set";
 import {V1x1ConfigurationSet} from "./v1x1_configuration_set";
-import {V1x1Configuration} from "./v1x1_configuration";
 import {V1x1ConfigurationDefinition} from "./v1x1_configuration_definition";
 export class V1x1Module {
   name: string;
   configurationDefinitionSet: V1x1ConfigurationDefinitionSet;
-  configurationSet: V1x1ConfigurationSet;
 
-  constructor(name: string, displayName: string, description: string, configurationDefinitionSet: V1x1ConfigurationDefinitionSet) {
+  constructor(name: string, configurationDefinitionSet: V1x1ConfigurationDefinitionSet) {
     this.name = name;
     this.configurationDefinitionSet = configurationDefinitionSet;
-    this.configurationSet = new V1x1ConfigurationSet(new V1x1Configuration({}), new V1x1Configuration({}), new V1x1Configuration({}));
   }
 
-  dirty() {
-    return this.configurationSet.global.dirty()
-        || this.configurationSet.tenant.dirty()
-        || this.configurationSet.channel.dirty();
+  dirty(configurationSet: V1x1ConfigurationSet) {
+    if(configurationSet === null || configurationSet === undefined)
+      return false;
+    return configurationSet.global.dirty()
+        || configurationSet.tenant.dirty()
+        || configurationSet.channel.dirty();
   }
 
   get displayName(): string {
