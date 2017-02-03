b068d646537f973dc0ab3b0495848c233e1af964
Cobi <cobi@jacobicarter.com>
2017-02-03 05:10:33 -0500

    +Web remember auth +user drop down

--- v1x1-web/app/components/nav/user_dropdown.ts
+++ v1x1-web/app/components/nav/user_dropdown.ts
@@ -1,15 +1,15 @@
 import {Component} from "@angular/core";
+import {V1x1Api} from "../../services/api";
+import {V1x1GlobalUser} from "../../model/v1x1_global_user";
 @Component({
   selector: 'user-dropdown-nav-component',
   template: `
-    <li class="nav-item" ngbDropdown *ngIf="loggedIn">
+    <li class="nav-item" ngbDropdown>
       <a class="nav-link" href="#" id="navbarUserDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ngbDropdownToggle>
         User
       </a>
-      <div class="dropdown-menu" style="left: auto; right: 0;" aria-labelledby="navbarUserDropdownMenuLink">
-        <span class="dropdown-item"><span class="fa fa-twitch"></span>JacobiCarter</span>
-        <span class="dropdown-item"><span class="fa fa-twitch"></span>JacobiCarterAFK</span>
-        <span class="dropdown-item">Cobi#8701</span>
+      <div class="dropdown-menu" style="left: auto; right: 0;" aria-labelledby="navbarUserDropdownMenuLink" *ngIf="globalUser !== null">
+        <span class="dropdown-item" *ngFor="let user of globalUser.users"><span class="fa" [class.fa-twitch]="user.platform === 'TWITCH'">/</span>{{user.displayName}}</span>
         <hr>
         <a class="dropdown-item" href="#">Link/Unlink Users</a>
         <a class="dropdown-item" href="#">Switch Users</a>
@@ -18,5 +18,9 @@ import {Component} from "@angular/core";
   `
 })
 export class UserDropdownNavComponent {
+  globalUser: V1x1GlobalUser = null;
 
+  constructor(private api: V1x1Api) {
+    this.api.getSelf().subscribe(r => this.globalUser = r);
+  }
 }
---
+++ v1x1-web/app/model/v1x1_api_string.ts
@@ -0,0 +1,6 @@
+import {JsonObject, JsonProperty} from "json2typescript";
+@JsonObject
+export class V1x1ApiString {
+  @JsonProperty("value")
+  value: string = undefined;
+}
