42f09ef74f156b41255a3d5a91b846b45695f033
Jacobi Carter <cobi@jacobicarter.com>
2017-02-03 02:20:41 -0500

    Web WIP

--- v1x1-web/app/model/v1x1_user.ts
+++ v1x1-web/app/model/v1x1_user.ts
@@ -1,3 +1,12 @@
+import {JsonObject, JsonProperty} from "json2typescript";
+@JsonObject
 export class V1x1User {
-
+  @JsonProperty("global_user_id")
+  globalUserId: string = undefined;
+  @JsonProperty("platform")
+  platform: string = undefined;
+  @JsonProperty("user_id")
+  userId: string = undefined;
+  @JsonProperty("display_name")
+  displayName: string = undefined;
 }
