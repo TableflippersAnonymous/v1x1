42eafa14b37e7925fbf8baa5c9edff6df0a8e42d
Cobi <cobi@jacobicarter.com>
2018-05-16 04:51:26 -0400

    Fix spacing with latest FontAwesome.

--- v1x1-web/app/components/util/platform_formatter.ts
+++ v1x1-web/app/components/util/platform_formatter.ts
@@ -4,7 +4,7 @@ import {Component, Input} from "@angular/core";
   selector: 'platform-formatter',
   template: `
     <span [class.color-twitch]="platform === 'TWITCH'" [class.color-discord]="platform === 'DISCORD'">
-      <i [class.fa-twitch]="platform === 'TWITCH'" [class.fa-discord]="platform === 'DISCORD'" [class.fab]="true"></i> <ng-content></ng-content>
+      <i [class.fa-twitch]="platform === 'TWITCH'" [class.fa-discord]="platform === 'DISCORD'" [class.fab]="true"></i>&nbsp;<ng-content></ng-content>
     </span>
   `
 })
