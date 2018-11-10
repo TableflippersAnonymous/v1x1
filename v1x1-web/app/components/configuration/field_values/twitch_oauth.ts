3fe4e85096a505ecb5956134658705e9ec5b670b
Cobi <cobi@jacobicarter.com>
2018-11-10 16:16:21 -0500

    Handle supplying oauth-tokens as strings until oauth-flow code is written.

--- v1x1-web/app/components/configuration/field_values/twitch_oauth.ts
+++ v1x1-web/app/components/configuration/field_values/twitch_oauth.ts
@@ -1,10 +1,24 @@
 import {Component, Input} from "@angular/core";
 import {V1x1ConfigurationDefinitionField} from "../../../model/v1x1_configuration_definition_field";
 import {ConfigurableComponent} from "../configurable";
+
 @Component({
   selector: 'configuration-field-value-twitch-oauth',
-  template: `twitch-oauth`
+  template: `
+    <span>
+      <mat-form-field class="input-group">
+        <input matInput type="text" [ngModel]="configuration" (blur)="configuration = $event.target.value" class="form-control" placeholder="{{field.displayName}}">
+      </mat-form-field>
+      <span class="input-group-append" *ngIf="configDirty() && originalValid()">
+        <button mat-button color="accent" (click)="abandonChanges()"><i class="far fa-undo"></i></button>
+      </span>
+    </span>
+  `
 })
 export class ConfigurationFieldValueTwitchOauthComponent extends ConfigurableComponent {
   @Input() public field: V1x1ConfigurationDefinitionField;
+
+  originalValid() {
+    return typeof this.originalConfiguration === 'string';
+  }
 }
